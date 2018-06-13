5a7d928d03295ee30107aba3202a432b83c95944
fwcd <fwcdmail@gmail.com>
2018-06-13 18:38:40 +0200

    Created a mixed Kotlin-Java project

---
+++ src/main/java/JavaApp.java
@@ -0,0 +1,6 @@
+public class JavaApp {
+	public static void javaHelloWorld() {
+		System.out.println("Hello world from Java");
+		KotlinApp.kotlinHelloWorld();
+	}
+}
---
+++ src/main/kotlin/KotlinApp.kt
@@ -0,0 +1,5 @@
+object KotlinApp {
+	@JvmStatic fun kotlinHelloWorld() {
+		println("Hello world from Kotlin")
+	}
+}

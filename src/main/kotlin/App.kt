1e54b10978cddcc56172e523f5a18605ba2b76a1
fwcd <fwcdmail@gmail.com>
2018-06-13 18:30:17 +0200

    Some basic functions for debug testing

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,17 @@
+fun pow(a: Int, b: Int): Int {
+	var result = 1
+
+	for (i in 0 until b) {
+		result *= a
+	}
+
+	return result
+}
+
+fun add(a: Int, b: Int): Int {
+	return a + b
+}
+
 fun main(args: Array<String>) {
-	println("Hello world")
+	println(pow(2, 4))
 }

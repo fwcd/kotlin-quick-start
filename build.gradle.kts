c4e8d75398d15bb1292e3218bca0a489ca8fcdd6
fwcd <fwcdmail@gmail.com>
2022-04-26 21:44:28 +0200

    Update Kotlin to 1.6.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,15 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.71"
+    id("org.jetbrains.kotlin.jvm") version "1.6.20"
 
     // Apply the application plugin to add support for building a CLI application.
     application
 }
 
 repositories {
-    // Use jcenter for resolving dependencies.
     // You can declare any Maven/Ivy/file repository here.
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
